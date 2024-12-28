7ac935582699e77539510bc75e3b385c0f60fc05
Dmytro Horodyskyi <dimagorodiskij@gmail.com>
2024-12-28 09:02:41 +0200

    Initial Commit

---
+++ Rick and Morty Characters/Rick_and_Morty_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_CharactersApp.swift
+//  Rick and Morty Characters
+//
+//  Created by Dmytro Horodyskyi on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
